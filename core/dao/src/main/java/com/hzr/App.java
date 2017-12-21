71eb5df6e0445a8f730a352a0509f6bbcf4ee06e
hzr <497494145@qq.com>
2017-12-21 22:36:34 +0800

    stash

--- core/dao/src/main/java/com/hzr/App.java
+++ core/dao/src/main/java/com/hzr/App.java
@@ -1,5 +1,7 @@
 package com.hzr;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -12,6 +14,7 @@ public class App
         System.out.println("test");
         for (int i = 0; i < 100; i++) {
             System.err.println("i :"+ i);
+            final Date date = new Date();
         }
     }
 }
